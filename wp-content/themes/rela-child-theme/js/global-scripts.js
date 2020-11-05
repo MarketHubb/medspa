a220da05631239423e0a78cfc5e8532b6cd4d784
Chris Hubbard <chris@markethubb.com>
2020-11-04 19:13:26 -0500

    Update: Global show/hide helper classes + theme styles

--- wp-content/themes/rela-child-theme/js/global-scripts.js
+++ wp-content/themes/rela-child-theme/js/global-scripts.js
@@ -7,11 +7,19 @@
 
     //region Window Load
     $(window).load(function() {
+
+        // Replace PNG logo with SVG
         $('.main-header__logo a img').attr({
            'src': LogoSvg,
            'srcset': LogoSvg
         });
     });
+
+    // Create custom show/hide functionality
+    $('.show-hide .aheto-list__title').on('click', function() {
+       $(this).next().slideToggle();
+    });
+
     //endregion
 
 })( jQuery );
